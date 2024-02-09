033e0f3d8d0c6230831db1b68b132efc7e7acc23
Joao Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-02-09 10:03:42 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleInferencia
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /// Warning Var em vez de tipo não é uma boa prática
+            var x = 10;
+            var y = 10.0;
+            var z = " Joao";
+
+            Console.WriteLine(x + y + z);
+        }
+    }
+}
